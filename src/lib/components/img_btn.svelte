068e3e08031e39f9dfd2cd91a56a360c41ab66be
davilivob <david.iii.chiu@gmail.com>
2025-08-18 21:29:47 +0800

    Update img_btn.svelte

--- src/lib/components/img_btn.svelte
+++ src/lib/components/img_btn.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
+	interface Picture {
+		sources: Record<string, string>;
+		img: {
+			src: string;
+			w: number;
+			h: number;
+		};
+	}
+
 	interface Props {
-		src: string;
+		src: string | { idle: string; touched: string };
+		srcPreprocessor?: (src: string) => string | Picture;
 		onclick?: () => void;
 		ontouchstart?: () => void;
 		ontouchend?: () => void;
+		scale?: number;
 		type?: 'button' | 'submit' | 'reset' | null;
 		form?: string;
 		class?: string;
@@ -12,12 +23,15 @@
 	let {
 		class: className,
 		src,
+		srcPreprocessor = (src) => src,
 		onclick = () => {},
+		scale = 0.9,
 		ontouchstart,
 		ontouchend,
 		...others
 	}: Props = $props();
 
+	let touched = $state(false);
 	let img = $state<HTMLImageElement>();
 
 	function scaleImg(num: number) {
@@ -32,13 +46,24 @@
 	{...others}
 	{onclick}
 	ontouchstart={() => {
-		scaleImg(0.9);
+		scaleImg(scale);
+		touched = true;
 		ontouchstart?.();
 	}}
 	ontouchend={() => {
 		scaleImg(1);
+		touched = false;
 		ontouchend?.();
 	}}
 >
-	<enhanced:img bind:this={img} {src} alt="" class="w-full transition-transform duration-100" />
+	<enhanced:img
+		bind:this={img}
+		src={typeof src === 'string'
+			? srcPreprocessor(src)
+			: touched
+				? srcPreprocessor(src.touched)
+				: srcPreprocessor(src.idle)}
+		alt=""
+		class="w-full transition-transform duration-100"
+	/>
 </button>
