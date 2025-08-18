c5bb4d1c72b10ef27c04ec264981a1be9444c381
davilivob <david.iii.chiu@gmail.com>
2025-08-18 21:32:41 +0800

    stop export ImgBtn

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,4 +1,4 @@
-export { default as ImgBtn } from './img_btn.svelte';
+// export { default as ImgBtn } from './img_btn.svelte';
 export { default as Marquee } from './marquee.svelte';
 export { default as FullscreenChecker } from './fullscreen_checker.svelte';
 export { default as TreeView } from './tree_view.svelte';
