11ab29b4e0be226c9810a44a848ff38660ca9863
davilivob <david.iii.chiu@gmail.com>
2025-08-18 21:30:04 +0800

    reorder plugins in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 
 export default defineConfig({
-	plugins: [sveltekit(), enhancedImages()],
+	plugins: [enhancedImages(), sveltekit()],
 	server: { port: 5566 }
 });
