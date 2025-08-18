24747ed216235fec4362d0d93511725629ff904d
davilivob <david.iii.chiu@gmail.com>
2025-08-18 21:20:38 +0800

    [tw-plugin] prettify

--- src/lib/tw-plugin/mobile_lock.ts
+++ src/lib/tw-plugin/mobile_lock.ts
@@ -3,7 +3,7 @@ import plugin from 'tailwindcss/plugin';
 export default plugin(({ addBase }) => {
 	addBase({
 		'touch-action': 'pan-x pan-y',
-		overflow: 'hidden',
+		'overflow': 'hidden',
 		'-webkit-user-select': 'none' /* Safari */,
 		'-ms-user-select': 'none' /* IE 10 and IE 11 */,
 		'user-select': 'none' /* Standard syntax */
